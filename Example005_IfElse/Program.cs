39df16f3b77d628457c57c77f89a4e2763bc730e
Stanislav <stanich81@gmail.com>
2023-10-11 00:06:12 +0300

    Алгоритм приветствия

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("О, привет, Маша!!!");
+}
+else
+{
+    Console.Write("Опять ты, ");
+    Console.Write(username);
+    Console.Write("?");
+}
