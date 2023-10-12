af8d53131ca8ac97e6ad83d72e1bae7abb28d15c
Stanislav <stanich81@gmail.com>
2023-10-12 23:04:40 +0300

    программка на поиск квадрата числа

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.Write("Введите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int sqr = number * number;
-Console.Write("Квадрат числа равен: ");
-System.Console.WriteLine(sqr);
-
-
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sqr = number2 * number2;
+if (number1 == sqr) Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+else Console.WriteLine($"Число {number1} НЕ является квадратом числа {number2}");
