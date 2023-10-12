045752bb084d593242e0b9ad91fc59caab8c4a43
Stanislav <stanich81@gmail.com>
2023-10-12 21:21:26 +0300

    создали Семинар 1

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,42 +1,37 @@
-﻿Console.Clear();
-// Console.SetCursorPosition(10, 4);
-// Console.WriteLine("*");
-int xa = 50, ya = 1, 
-    xb = 1, yb = 30,
-    xc = 100, yc = 30;
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("*");
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("*");
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("*");
+﻿using System;
 
-int x = xa, y = ya;
-int count = 0;
-
-while(count < 1000)
-{
-    int what = new Random().Next(0, 3); // 0, 1, 2
-    if(what == 0)
+namespace Serpinski 
+{ 
+    class Program
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        static void Main(string[] args)
+        {
+            Console.Write("Power of 2: ");
+            int depth = Convert.ToInt32(Math.Pow(2d, Convert.ToDouble(Console.ReadLine())));
+
+            int[][] pascaltriangle = new int[depth][];
+            for (int i = 0; i < pascaltriangle.Length; i++)
+            {
+                pascaltriangle[i] = new int[depth];
+                for (int j = 0; j < pascaltriangle[i].Length; j++)
+                    pascaltriangle[i][j] = 0;
+                pascaltriangle[i][0] = 1;
+                pascaltriangle[i][i] = 1;
+            }
+
+            for (int i = 1; i < pascaltriangle.Length; i++)
+                for (int j = 1; j < pascaltriangle[i].Length; j++)
+                    pascaltriangle[i][j] = (pascaltriangle[i - 1][j - 1] + pascaltriangle[i - 1][j]) % 2;
+
+            for (int i = 0; i < pascaltriangle.Length; i++)
+            {
+                for (int j = 0; j < pascaltriangle[i].Length; j++)
+                    Console.Write(pascaltriangle[i][j] == 1 ? "#" : " ");
+                Console.WriteLine();
+            }
+            
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
     }
-
-    if(what == 1)
-    {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
-    }
-
-    if(what == 2)
-    {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
-    }
-
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("*");
-    count++;
-} 
-
+}
