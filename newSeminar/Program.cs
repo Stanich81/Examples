62f7b4e45c426498c258b1157d656f7687fefa9c
Stanislav <stanich81@gmail.com>
2023-11-17 23:20:29 +0300

    возник вопрос с переменными min и max

---
+++ newSeminar/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 6;
+int b = a*(-1);
+
+if (a>b){
+    int max = a; int min = b;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+else {
+    int max = b; int min = a;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+while(b < a){
+    Console.Write($"{b}, ");
+    b = b + 1; 
+}
+Console.Write($"{b}"); //убираем запятую после последней цифры
+
+//while(min < max){
+//    Console.Write($"{min}, ");
+//    int min = min + 1; 
+//}
+//Console.Write($"{min}");
